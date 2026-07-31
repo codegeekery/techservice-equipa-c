84864225959e6bc2418ac230bc70d8bc43e92886
Joaquim Santos <Joaquim.santos1493@gmail.com>
2026-07-31 18:53:04 +0100

    Placeholder Name

--- src/service/estado_service.py
+++ src/service/estado_service.py
@@ -5,7 +5,7 @@ Responsável pelas regras de negócio, validações e transições de estado das
 
 from typing import List, Dict, Optional
 import src.repositories.estados_OS_repository as estado_repo
-from src.models.estado import Estado
+from src.models.estado import Estado #Placeholder Name
 
 
 class EstadoService:
