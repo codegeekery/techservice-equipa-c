236b04555369ec77a4ef6134178213ac94546ae0
ricardogomes-coder <ricardogomesgpsi@gmail.com>
2026-07-31 18:51:18 +0100

    estados_OS

--- src/repositories/estados_OS_repository.py
+++ src/repositories/estados_OS_repository.py
@@ -1,42 +1,94 @@
 from src.database.conexao import conectar
 
-def adicionar_estado(nome, descricao, ordem, ativo):
-    conn = conectar()
-    cursor = conn.cursor()
+def adicionar(estado):
+    conexao = conectar()
+    cursor = conexao.cursor()
 
     sql = """
-    INSERT INTO estados_os (nome, descricao, ordem, ativo)
-    VALUES (%s, %s, %s, %s)
+        INSERT INTO estados_os 
+        (nome, descricao, ordem, ativo)
+        VALUES (%s, %s, %s, %s)
     """
 
-    cursor.execute(sql, (nome, descricao, ordem, ativo))
-    conn.commit()
+    valores = (
+        estado.get_nome(),
+        estado.get_descricao(),
+        estado.get_ordem(),
+        estado.get_ativo()
+    )
 
-    print("Estado cadastrado com sucesso!")
+    cursor.execute(sql, valores)
+    conexao.commit()
+
+    estado.id_estado = cursor.lastrowid
 
     cursor.close()
-    conn.close()
+    conexao.close()
 
+    return estado
 
 def listar_estado(id_estado):
-    conn = conectar()
-    cursor = conn.cursor(dictionary=True)
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
 
     sql = """
-    SELECT
-        id_estado,
-        nome,
-        descricao,
-        ordem,
-        ativo
-    FROM estados_os
-    WHERE id_estado = %s
+        SELECT *
+        FROM estados_os
+        WHERE id_estado = %s
     """
 
     cursor.execute(sql, (id_estado,))
+
     estado = cursor.fetchone()
 
     cursor.close()
-    conn.close()
+    conexao.close()
 
     return estado
+
+def atualizar(estado):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = """
+        UPDATE estados_os
+        SET nome=%s,
+            descricao=%s,
+            ordem=%s,
+            ativo=%s
+        WHERE id_estado=%s
+    """
+
+    valores = (
+        estado.get_nome(),
+        estado.get_descricao(),
+        estado.get_ordem(),
+        estado.get_ativo(),
+        estado.get_id_estado()
+    )
+
+    cursor.execute(sql, valores)
+
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    return estado
+
+def eliminar_estado(id_estado):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = """
+        DELETE FROM estados_os
+        WHERE id_estado = %s
+    """
+
+    cursor.execute(sql, (id_estado,))
+
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
